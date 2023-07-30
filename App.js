6b40825be075ec239a3670a8283f6d8159ee03a7
veronika sukantha <85723282+veronika-sukantha@users.noreply.github.com>
2023-07-30 20:03:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Calci from './Components/Calci';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Calci />
+    </div>
+  );
+}
+
+export default App;
